05c51ba52a96cac09c983c170b09d7cae13d1fae
Olivier7355 <108932082+Olivier7355@users.noreply.github.com>
2023-01-09 15:28:21 +0100

    Update Defibrillators.py

--- Easy/Defibrillators.py
+++ Easy/Defibrillators.py
@@ -1,1 +1,46 @@
+# https://www.codingame.com/ide/puzzle/defibrillators
 
+import sys
+import math
+
+lon = float(input().replace(',', '.'))
+lat = float(input().replace(',', '.'))
+n = int(input())    # Number N of defibrillators
+
+defibralatorList =[]
+
+for i in range(n):
+    defib = input()
+
+    # split each decription to extract : Name, Longitude (degrees), Latitude (degrees)
+    defibList = defib.split(';')
+
+    # Convert Longitude (degrees), Latitude (degrees) from string to floats
+    # replace the , with a .
+    # Converts Longitude (degrees), Latitude (degrees) in radians
+    defibList[-1] = math.radians(float(defibList[-1].replace(',', '.')))
+    defibList[-2] = math.radians(float(defibList[-2].replace(',', '.')))
+
+    # Calculate his distance from lon and lat
+    x = (defibList[-2] - math.radians(lon)) * math.cos((math.radians(lat) + defibList[-1])/2)
+    y = (defibList[-1] - (math.radians(lat)))
+    distance = math.sqrt(x**2 + y**2) * 6371
+    defibList.append(distance)
+
+    # Append to the defibralatorList
+    defibralatorList.append(defibList )
+
+# nearest = the first distance from defibralatorList
+nearest = defibralatorList[0][-1]
+answer = defibralatorList[0][1]
+
+#print("", file=sys.stderr, flush=True)
+# For each defibrilator in defibralatorList :
+for i in range(len(defibralatorList)) :
+
+    # if distance is smallest than the one in nearest :  nearest = d and answer = adress of the defibrilator
+    if defibralatorList[i][-1] < nearest :
+        nearest = defibralatorList[i][-1]
+        answer = defibralatorList[i][1]
+
+print(answer)
